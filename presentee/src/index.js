743a74c9058822fc469dbc7a67e98cbddaa3d40c
Christopher Liudahl-Rackley <53634121+clrminded@users.noreply.github.com>
2022-10-07 22:21:31 -0700

    trying to add tailwind

--- presentee/src/index.js
+++ presentee/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-
+import './index.css';
 import App from './App';
+
+
+// hook to root div
+ReactDOM.render(<App />, document.getElementById('root'));
